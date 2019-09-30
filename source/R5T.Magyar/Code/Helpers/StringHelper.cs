7d45879697c2c1cbf7e9cc821dec1c568bfc68b2
MinexAutomation <minex.automation@gmail.com>
2019-09-30 11:37:13 -0700

    Added string helper class containing easy is-found functionality for use with the String.Contains() method.

---
+++ source/R5T.Magyar/Code/Helpers/StringHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace R5T.Magyar
+{
+    public static class StringHelper
+    {
+        public const string Invalid = null;
+
+        /// <summary>
+        /// The return value of the <see cref="String.IndexOf(string)"/> method when the search string is not found.
+        /// </summary>
+        public const int IndexOfNotFound = -1;
+
+
+        /// <summary>
+        /// Useful for testing the return value of the <see cref="String.IndexOf(string)"/> method to see if the search string was found.
+        /// </summary>
+        public static bool IsFound(int index)
+        {
+            var isFound = index != StringHelper.IndexOfNotFound;
+            return isFound;
+        }
+
+        public static bool IsValid(string value)
+        {
+            var output = value != StringHelper.Invalid;
+            return output;
+        }
+    }
+}
