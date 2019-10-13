b2f51301db8bb8077b7876f8cf14b8b1a1ae991e
MinexAutomation <minex.automation@gmail.com>
2019-10-13 09:28:37 -0700

    Added search-option and search-pattern helper functionality.

---
+++ source/R5T.Magyar/Code/Helpers/SearchOptionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+
+namespace R5T.Magyar
+{
+    public static class SearchOptionHelper
+    {
+        public static SearchOption RecursiveToSearchOption(bool recursive)
+        {
+            if(recursive)
+            {
+                return SearchOption.AllDirectories;
+            }
+            else
+            {
+                return SearchOption.TopDirectoryOnly;
+            }
+        }
+    }
+}
---
+++ source/R5T.Magyar/Code/Helpers/SearchPatternHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Magyar
+{
+    public static class SearchPatternHelper
+    {
+        public const string All = "*";
+    }
+}
