a4d211f0b2b13d5882c70661452c9fe1a37637ef
MinexAutomation <minex.automation@gmail.com>
2019-12-06 20:12:34 -0800

    Added StringBuilder Trim() and TrimLast() extensions.

---
+++ source/R5T.Magyar/Code/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+
+namespace R5T.Magyar
+{
+    public static class StringBuilderExtensions
+    {
+        public static void Trim(this StringBuilder stringBuilder, int count)
+        {
+            stringBuilder.Remove(stringBuilder.Length - count, count);
+        }
+
+        public static void TrimLast(this StringBuilder stringBuilder)
+        {
+            stringBuilder.Trim(1);
+        }
+    }
+}
