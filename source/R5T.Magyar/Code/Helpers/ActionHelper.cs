d25227df8b74d9860e19f19d62327951baab915c
MinexAutomation <minex.automation@gmail.com>
2019-12-12 23:33:24 -0800

    Added static ActionHelper Action helper.

---
+++ source/R5T.Magyar/Code/Helpers/ActionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace R5T.Magyar
+{
+    public static class ActionHelper
+    {
+        /// <summary>
+        /// Does nothing.
+        /// This is the "empty" action.
+        /// </summary>
+        public static void DoNothing()
+        {
+            // Do nothing.
+        }
+
+        /// <summary>
+        /// Does nothing with input.
+        /// This is the "empty" action.
+        /// </summary>
+        public static void DoNothing<T>(T _)
+        {
+            // Do nothing.
+        }
+    }
+}
