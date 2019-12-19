c940b6c76e37220a702f4963d0385245ca2aa9d6
MinexAutomation <minex.automation@gmail.com>
2019-12-19 13:23:03 -0800

    Added IsFile() and GetParentDirectoryPath() extension methods.

--- source/R5T.Magyar/Code/IO/Helpers/FileHelper.cs
+++ source/R5T.Magyar/Code/IO/Helpers/FileHelper.cs
@@ -21,5 +21,24 @@ namespace R5T.Magyar.IO
             var output = File.Exists(filePath);
             return output;
         }
+
+        /// <summary>
+        /// Determine if a path is a file.
+        /// </summary>
+        public static bool IsFile(string path)
+        {
+            var isDirectory = DirectoryHelper.IsDirectory(path);
+
+            var isFile = !isDirectory; // Assumes that file-system entries are either files, or directories.
+            return isFile;
+        }
+
+        public static string GetParentDirectoryPath(string filePath)
+        {
+            var fileInfo = new FileInfo(filePath);
+
+            var parentDirectoryPath = fileInfo.Directory.FullName;
+            return parentDirectoryPath;
+        }
     }
 }
