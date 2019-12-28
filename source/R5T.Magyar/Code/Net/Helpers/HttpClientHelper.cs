10710fc76f4b4f6663b4a77f1429c2bced20f8c6
MinexAutomation <minex.automation@gmail.com>
2019-12-28 12:52:12 -0800

    Added a static HttpClientHelper in the R5T.Magyar.Net namespace. Founded the R5T.Magyar.Net namespace.

---
+++ source/R5T.Magyar/Code/Net/Helpers/HttpClientHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace R5T.Magyar.Net
+{
+    public static class HttpClientHelper
+    {
+        public static async Task<string> GetString(string url)
+        {
+            using(var client = new HttpClient())
+            {
+                var result = await client.GetStringAsync(url);
+                return result;
+            }
+        }
+    }
+}
