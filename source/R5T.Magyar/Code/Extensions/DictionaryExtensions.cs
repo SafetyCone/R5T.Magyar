7ea7332f1b68da8131222408a3bfd159a513d99f
MinexAutomation <minex.automation@gmail.com>
2020-01-05 13:20:00 -0800

    Added Copy() Dictionary extension.

---
+++ source/R5T.Magyar/Code/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<TKey, TValue> Copy<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            var output = new Dictionary<TKey, TValue>();
+
+            dictionary.ForEach(keyValuePair =>
+            {
+                output.Add(keyValuePair.Key, keyValuePair.Value);
+            });
+
+            return output;
+        }
+    }
+}
