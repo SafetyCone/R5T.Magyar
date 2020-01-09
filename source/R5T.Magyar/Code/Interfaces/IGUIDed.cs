cca393053a03cb5cbb98f22bfdf0f63c5155f955
MinexAutomation <minex.automation@gmail.com>
2020-01-09 14:04:48 -0800

    Added IIDed and IGUIDed interface type definitions.

---
+++ source/R5T.Magyar/Code/Interfaces/IGUIDed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Magyar
+{
+    /// <summary>
+    /// Has a GUID (Globally Unique ID).
+    /// </summary>
+    public interface IGUIDed
+    {
+        Guid GUID { get; }
+    }
+}
---
+++ source/R5T.Magyar/Code/Interfaces/IIDed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Magyar
+{
+    /// <summary>
+    /// Has an ID.
+    /// </summary>
+    public interface IIDed
+    {
+        int ID { get; }
+    }
+}
