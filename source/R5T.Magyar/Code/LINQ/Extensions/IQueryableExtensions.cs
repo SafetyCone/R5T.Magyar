becbe5e29d3ae6e7764f0ad24b81a238f67b8b2a
MinexAutomation <minex.automation@gmail.com>
2020-01-09 14:06:26 -0800

    Added IQueryable extensions.

---
+++ source/R5T.Magyar/Code/LINQ/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+
+namespace R5T.Magyar.LINQ
+{
+    public static class IQueryableExtensions
+    {
+        public static HasOutput<T> HasSingle<T>(this IQueryable<T> queryable)
+            where T: class
+        {
+            var singleOrDefault = queryable.SingleOrDefault();
+
+            var hasOutput = HasOutput.FromReferenceType(singleOrDefault);
+            return hasOutput;
+        }
+
+        public static HasOutput<T> HasSingle<T>(this IQueryable<T> queryable, T nonExistentValue)
+            where T : struct
+        {
+            var singleOrDefault = queryable.SingleOrDefault();
+
+            var hasOutput = HasOutput.FromValueType(singleOrDefault, nonExistentValue);
+            return hasOutput;
+        }
+    }
+}
