640bd0080aaa575671894d505c219723cc73c89e
MinexAutomation <minex.automation@gmail.com>
2020-01-23 16:07:30 -0800

    Added TextReader extensions.

---
+++ source/R5T.Magyar/Code/IO/Extensions/TextReaderExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+
+namespace R5T.Magyar.IO.Extensions
+{
+    public static class TextReaderExtension
+    {
+        public static bool ReadLine(this TextReader textReader, out string line)
+        {
+            var output = TextReaderHelper.ReadLine(textReader, out line);
+            return output;
+        }
+    }
+}
