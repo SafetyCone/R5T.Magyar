72f4957574d18a362d89b10cb6dc7fcc5a7be449
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:13:19 -0800

    Added IList<T> extensions.

---
+++ source/R5T.Magyar/Code/Extensions/IListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar.Extensions
+{
+    public static class IListExtensions
+    {
+        public static void RemoveAll<T>(this IList<T> list, IEnumerable<T> enumerable)
+        {
+            foreach (var item in enumerable)
+            {
+                list.Remove(item);
+            }
+        }
+    }
+}
