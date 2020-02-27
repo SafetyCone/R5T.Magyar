2a847002f9e5ce204c05b887af32c05736ec6d10
MinexAutomation <minex.automation@gmail.com>
2020-02-27 00:16:55 -0800

    Added EnumerableHelper.

---
+++ source/R5T.Magyar/Code/Helpers/EnumerableHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar
+{
+    public static class EnumerableHelper
+    {
+        public static IEnumerable<T> From<T>(T value)
+        {
+            yield return value;
+        }
+    }
+}
