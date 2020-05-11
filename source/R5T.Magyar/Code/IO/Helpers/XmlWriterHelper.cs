c4471ac555dba49313d2b819886a78621e6a6c15
MinexAutomation <minex.automation@gmail.com>
2020-05-10 23:41:52 -0700

    Added XmlWriterHelper and added more settings configurations to XmlWriterSettingsHelper.

---
+++ source/R5T.Magyar/Code/IO/Helpers/XmlWriterHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+
+namespace R5T.Magyar.IO
+{
+    public static class XmlWriterHelper
+    {
+        /// <summary>
+        /// The System XML writer includes an XML declaration by default, however this is often not desired.
+        /// An XML writer can be created with settings specifying to omit the XML declaration, but other settings must be set to get the desired default behavior.
+        /// This method produces an XML writer that omits the declaration.
+        /// </summary>
+        public static XmlWriter NewOmitDeclaration(Stream stream)
+        {
+            var settings = XmlWriterSettingsHelper.GetNoDeclaration();
+
+            var writer = XmlWriter.Create(stream, settings);
+            return writer;
+        }
+
+        /// <summary>
+        /// Gets the standard XML writer.
+        /// </summary>
+        public static XmlWriter New(Stream stream)
+        {
+            var settings = XmlWriterSettingsHelper.GetStandardSettings();
+
+            var writer = XmlWriter.Create(stream, settings);
+            return writer;
+        }
+
+        public static XmlWriter New(string xmlFilePath)
+        {
+            var settings = XmlWriterSettingsHelper.GetStandardSettings();
+
+            var writer = XmlWriter.Create(xmlFilePath, settings);
+            return writer;
+        }
+    }
+}
--- source/R5T.Magyar/Code/IO/Helpers/XmlWriterSettingsHelper.cs
+++ source/R5T.Magyar/Code/IO/Helpers/XmlWriterSettingsHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Xml;
 
 
@@ -17,6 +18,34 @@ namespace R5T.Magyar.IO
                 Indent = true,
             };
             return xmlWriterSettings;
+        }
+
+        /// <summary>
+        /// The System XML writer includes an XML declaration by default, however this is often not desired.
+        /// An XML writer can be created with settings specifying to omit the XML declaration, but other settings must be set to get the desired default behavior.
+        /// This method produces XML writer settings that replicate the default settings, except specifying to omit the declaration.
+        /// </summary>
+        public static XmlWriterSettings GetNoDeclaration()
+        {
+            var settings = new XmlWriterSettings
+            {
+                OmitXmlDeclaration = true,
+            };
+
+            return settings;
+        }
+
+        /// <summary>
+        /// Gets the standard XML writer settings.
+        /// </summary>
+        public static XmlWriterSettings GetStandardSettings()
+        {
+            var settings = XmlWriterSettingsHelper.GetNoDeclaration();
+
+            settings.Encoding = new UTF8Encoding(false);
+            settings.Indent = true;
+
+            return settings;
         }
     }
 }
