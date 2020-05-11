89c50c7943649ce1227783b1bba3eaf1859f1e6a
MinexAutomation <minex.automation@gmail.com>
2020-05-11 15:37:08 -0700

    Added custom exceptions, integrated custom Exceptions with EnumerationHelper, and changed name of EnumerationHelper class.

---
+++ source/R5T.Magyar/Code/Exceptions/UnexpectedEnumerationValueException.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace R5T.Magyar
+{
+    public class UnexpectedEnumerationValueException<TEnum> : System.IO.FileNotFoundException
+        where TEnum: Enum
+    {
+        protected const string ValuePropertyName = "Value";
+
+
+        public TEnum Value { get; }
+
+
+        public UnexpectedEnumerationValueException()
+            : base()
+        {
+        }
+
+        public UnexpectedEnumerationValueException(string message)
+            : base(message)
+        {
+        }
+
+        public UnexpectedEnumerationValueException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected UnexpectedEnumerationValueException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            var valueStringRepresentation = info.GetString(UnexpectedEnumerationValueException<TEnum>.ValuePropertyName);
+
+            this.Value = EnumerationHelper.GetValue<TEnum>(valueStringRepresentation);
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue(UnexpectedEnumerationValueException<TEnum>.ValuePropertyName, this.Value.ToString());
+        }
+
+        public UnexpectedEnumerationValueException(TEnum value)
+            : this(EnumerationHelper.UnexpectedEnumerationValueMessage<TEnum>(value))
+        {
+            this.Value = value;
+        }
+
+        public UnexpectedEnumerationValueException(TEnum value, string message)
+            : this(message)
+        {
+            this.Value = value;
+        }
+
+        public UnexpectedEnumerationValueException(TEnum value, string message, Exception innerException)
+            : this(message, innerException)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ source/R5T.Magyar/Code/Exceptions/UnrecognizedEnumerationValueException.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace R5T.Magyar
+{
+    public class UnrecognizedEnumerationValueException : Exception
+    {
+        private const string EnumerationTypeFullNamePropertyName = "EnumerationTypeName";
+        private const string UnrecognizedValuePropertyName = "UnrecogizedValue";
+
+
+        public string EnumerationTypeFullName { get; }
+        public string UnrecognizedValue { get; }
+
+
+        public UnrecognizedEnumerationValueException()
+        {
+        }
+
+        public UnrecognizedEnumerationValueException(string message)
+            : base(message)
+        {
+        }
+
+        public UnrecognizedEnumerationValueException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected UnrecognizedEnumerationValueException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            this.EnumerationTypeFullName = info.GetString(UnrecognizedEnumerationValueException.EnumerationTypeFullNamePropertyName);
+            this.UnrecognizedValue = info.GetString(UnrecognizedEnumerationValueException.UnrecognizedValuePropertyName);
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue(UnrecognizedEnumerationValueException.EnumerationTypeFullNamePropertyName, this.EnumerationTypeFullName);
+            info.AddValue(UnrecognizedEnumerationValueException.UnrecognizedValuePropertyName, this.UnrecognizedValue);
+        }
+
+        public UnrecognizedEnumerationValueException(string enumerationTypeFullName, string unrecognizedValue)
+            : this(EnumerationHelper.UnrecognizedEnumerationValueMessage(enumerationTypeFullName, unrecognizedValue))
+        {
+            this.EnumerationTypeFullName = enumerationTypeFullName;
+            this.UnrecognizedValue = unrecognizedValue;
+        }
+
+        public UnrecognizedEnumerationValueException(string enumerationTypeFullName, string unrecognizedValue, string message)
+            : this(message)
+        {
+            this.EnumerationTypeFullName = enumerationTypeFullName;
+            this.UnrecognizedValue = unrecognizedValue;
+        }
+
+        public UnrecognizedEnumerationValueException(string enumerationTypeFullName, string unrecognizedValue, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            this.EnumerationTypeFullName = enumerationTypeFullName;
+            this.UnrecognizedValue = unrecognizedValue;
+        }
+    }
+}
