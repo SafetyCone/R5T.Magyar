879cbd91935d514e565c8e300ddc1683a4f91da3
MinexAutomation <minex.automation@gmail.com>
2020-05-12 16:45:23 -0700

    Added Result<T> and VersionHelper classes.

---
+++ source/R5T.Magyar/Code/Classes/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar
+{
+    public class Result<T>
+    {
+        public T Value { get; }
+        public IEnumerable<string> Messages { get; }
+
+
+        public Result(T value, IEnumerable<string> messages)
+        {
+            this.Value = value;
+            this.Messages = messages;
+        }
+    }
+}
---
+++ source/R5T.Magyar/Code/Helpers/VersionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.Magyar
+{
+    public static class VersionHelper
+    {
+        public static Version None = null;
+
+
+        public static bool IsNone(Version version)
+        {
+            var isNone = version == VersionHelper.None;
+            return isNone;
+        }
+    }
+}
