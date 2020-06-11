bde9ecbb8f721e8e22a61ffab643b87547ffab18
MinexAutomation <minex.automation@gmail.com>
2020-06-11 10:57:57 -0700

    Added Xml-related functionality.

---
+++ source/R5T.Magyar/Code/Xml/Classes/Base Classes/CustomXmlWriterBase.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Xml;
+
+
+namespace R5T.Magyar.Xml
+{
+    /// <summary>
+    /// Note: class is almost useless because XmlWriter is almost useless!
+    /// The problem is that there is no expansion point for after the closing '>' (alligator) of an element (opening element) is written. Thus there is no way to customize line spacing.
+    /// This class is preserved since work was required to create it.
+    /// </summary>
+    public class CustomXmlWriterBase : XmlWriter
+    {
+        protected XmlWriter XmlWriter { get; }
+
+
+        public CustomXmlWriterBase(XmlWriter xmlWriter)
+        {
+            this.XmlWriter = xmlWriter;
+        }
+
+        public override WriteState WriteState => this.XmlWriter.WriteState;
+
+        public override void Flush()
+        {
+            this.XmlWriter.Flush();
+        }
+
+        public override string LookupPrefix(string ns)
+        {
+            return this.XmlWriter.LookupPrefix(ns);
+        }
+
+        public override void WriteBase64(byte[] buffer, int index, int count)
+        {
+            this.XmlWriter.WriteBase64(buffer, index, count);
+        }
+
+        public override void WriteCData(string text)
+        {
+            this.XmlWriter.WriteCData(text);
+        }
+
+        public override void WriteCharEntity(char ch)
+        {
+            this.XmlWriter.WriteCharEntity(ch);
+        }
+
+        public override void WriteChars(char[] buffer, int index, int count)
+        {
+            this.XmlWriter.WriteChars(buffer, index, count);
+        }
+
+        public override void WriteComment(string text)
+        {
+            this.XmlWriter.WriteComment(text);
+        }
+
+        public override void WriteDocType(string name, string pubid, string sysid, string subset)
+        {
+            this.XmlWriter.WriteDocType(name, pubid, sysid, subset);
+        }
+
+        public override void WriteEndAttribute()
+        {
+            this.XmlWriter.WriteEndAttribute();
+        }
+
+        public override void WriteEndDocument()
+        {
+            this.XmlWriter.WriteEndDocument();
+        }
+
+        public override void WriteEndElement()
+        {
+            this.XmlWriter.WriteEndElement();
+        }
+
+        public override void WriteEntityRef(string name)
+        {
+            this.XmlWriter.WriteEntityRef(name);
+        }
+
+        public override void WriteFullEndElement()
+        {
+            this.XmlWriter.WriteFullEndElement();
+        }
+
+        public override void WriteProcessingInstruction(string name, string text)
+        {
+            this.XmlWriter.WriteProcessingInstruction(name, text);
+        }
+
+        public override void WriteRaw(char[] buffer, int index, int count)
+        {
+            this.XmlWriter.WriteRaw(buffer, index, count);
+        }
+
+        public override void WriteRaw(string data)
+        {
+            this.XmlWriter.WriteRaw(data);
+        }
+
+        public override void WriteStartAttribute(string prefix, string localName, string ns)
+        {
+            this.XmlWriter.WriteStartAttribute(prefix, localName, ns);
+        }
+
+        public override void WriteStartDocument()
+        {
+            this.XmlWriter.WriteStartDocument();
+        }
+
+        public override void WriteStartDocument(bool standalone)
+        {
+            this.XmlWriter.WriteStartDocument(standalone);
+        }
+
+        public override void WriteStartElement(string prefix, string localName, string ns)
+        {
+            this.XmlWriter.WriteStartElement(prefix, localName, ns);
+        }
+
+        public override void WriteString(string text)
+        {
+            this.XmlWriter.WriteString(text);
+        }
+
+        public override void WriteSurrogateCharEntity(char lowChar, char highChar)
+        {
+            this.XmlWriter.WriteSurrogateCharEntity(lowChar, highChar);
+        }
+
+        public override void WriteWhitespace(string ws)
+        {
+            this.XmlWriter.WriteWhitespace(ws);
+        }
+    }
+}
---
+++ source/R5T.Magyar/Code/Xml/Extensions/XElementExtensions.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace R5T.Magyar.Xml
+{
+    public static class XElementExtensions
+    {
+        public static string GetChildValueSingle(this XElement xElement, string childName)
+        {
+            var output = xElement.GetChildren(childName)
+                .Single()
+                .Value;
+
+            return output;
+        }
+
+        public static string GetChildValueFirst(this XElement xElement, string childName)
+        {
+            var output = xElement.GetChildren(childName)
+                .First()
+                .Value;
+
+            return output;
+        }
+
+        /// <summary>
+        /// Uses <see cref="XElementExtensions.GetChildValueSingle(XElement, string)"/> as the default.
+        /// </summary>
+        public static string GetChildValue(this XElement xElement, string childName)
+        {
+            var output = xElement.GetChildValueSingle(childName);
+            return output;
+        }
+
+        public static IEnumerable<XElement> GetChildren(this XElement xElement, string childName)
+        {
+            var output = xElement.Elements()
+                .Where(x => x.Name == childName);
+
+            return output;
+        }
+
+        public static bool HasChildSingle(this XElement xElement, string childName, out XElement child)
+        {
+            child = xElement.GetChildren(childName)
+                .SingleOrDefault();
+
+            var hasChild = XElementHelper.WasFound(child);
+            return hasChild;
+        }
+
+        /// <summary>
+        /// Uses <see cref="XElementExtensions.HasChildSingle(XElement, string, out XElement)"/> as the default.
+        /// </summary>
+        public static bool HasChild(this XElement xElement, string childName, out XElement child)
+        {
+            var hasChild = xElement.HasChildSingle(childName, out child);
+            return hasChild;
+        }
+
+        public static bool HasChild(this XElement xElement, string childName)
+        {
+            var hasChild = xElement.HasChild(childName, out var _);
+            return hasChild;
+        }
+
+        /// <summary>
+        /// Normalizes an <see cref="XElement"/>.
+        /// </summary>
+        /// <remarks>
+        /// Source: https://weblogs.asp.net/marianor/easy-way-to-compare-two-xmls-for-equality
+        /// </remarks>
+        public static XElement Normalize(this XElement xElement)
+        {
+            if (xElement.HasElements)
+            {
+                return new XElement(
+                    xElement.Name,
+                    xElement.Attributes()
+                        .Where(a => a.Name.Namespace == XNamespace.Xmlns)
+                        .OrderBy(a => a.Name.ToString()),
+                    xElement.Elements()
+                        .OrderBy(a => a.Name.ToString())
+                        .Select(e => e.Normalize()));
+            }
+
+            if (xElement.IsEmpty)
+            {
+                return new XElement(
+                    xElement.Name,
+                    xElement.Attributes()
+                        .OrderBy(a => a.Name.ToString()));
+            }
+
+            return new XElement(
+                xElement.Name,
+                xElement.Attributes()
+                    .OrderBy(a => a.Name.ToString()),
+                xElement.Value);
+        }
+    }
+}
