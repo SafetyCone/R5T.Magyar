82ac905a8b12c139f7e131f61d165f627ffd11b6
MinexAutomation <minex.automation@gmail.com>
2020-06-18 14:54:56 -0700

    Renamed ICollectionExtensions.

---
+++ source/R5T.Magyar/Code/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar.Extensions
+{
+    public static class ICollectionExtensions
+    {
+        /// <summary>
+        /// Different than <see cref="ICollection{T}.Clear"/>, this allows use of an <see cref="IEnumerable{T}"/> to specific items to remove.
+        /// </summary>
+        public static void RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> enumerable)
+        {
+            foreach (var item in enumerable)
+            {
+                collection.Remove(item);
+            }
+        }
+    }
+}
