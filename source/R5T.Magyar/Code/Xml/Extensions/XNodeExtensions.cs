38e0075e91c1d55ecb3e7a3fd0a04fd69b22d468
MinexAutomation <minex.automation@gmail.com>
2020-06-18 11:17:55 -0700

    Added XNode extensions.

---
+++ source/R5T.Magyar/Code/Xml/Extensions/XNodeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace R5T.Magyar.Xml
+{
+    public static class XNodeExtensions
+    {
+        public static bool IsText(this XNode xNode)
+        {
+            var isNotFound = XNodeHelper.IsNotFound(xNode);
+            if(isNotFound)
+            {
+                return false;
+            }
+
+            var isText = xNode.NodeType == XmlNodeType.Text;
+            return isText;
+        }
+    }
+}
