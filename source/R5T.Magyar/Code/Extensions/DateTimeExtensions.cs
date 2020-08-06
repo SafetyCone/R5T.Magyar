918a9d4882b604be6c6673c329cb289de37abd33
David Coats <david@getsafetycone.com>
2020-08-05 17:18:23 -0700

    Added a new 12-hour AM/PM DateTime.ToString() extension method.

--- source/R5T.Magyar/Code/Extensions/DateTimeExtensions.cs
+++ source/R5T.Magyar/Code/Extensions/DateTimeExtensions.cs
@@ -17,12 +17,27 @@ namespace R5T.Magyar.Extensions
             return output;
         }
 
+        public static string TohhMMSS_tt(this DateTime dateTime)
+        {
+            var output = $"{dateTime:hhmmss tt}";
+            return output;
+        }
+
         public static string ToYYYYMMDD_HHMMSS(this DateTime dateTime)
         {
             string yyyymmdd = dateTime.ToYYYYMMDD();
             string hhmmss = dateTime.ToHHMMSS();
 
             string output = $"{yyyymmdd}-{hhmmss}";
+            return output;
+        }
+
+        public static string ToYYYYMMDD_hhMMSS_tt(this DateTime dateTime)
+        {
+            string dateRepresentation = dateTime.ToYYYYMMDD();
+            string timeRepresentation = dateTime.TohhMMSS_tt();
+
+            string output = $"{dateRepresentation}-{timeRepresentation}";
             return output;
         }
 
