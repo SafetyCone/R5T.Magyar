de0e64fe9f33911af0f4a731dea0e42372ea1839
David Coats <david@getsafetycone.com>
2020-08-13 00:26:08 -0700

    Added internet-related constants.

---
+++ source/R5T.Magyar/Code/Net/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Magyar.Net
+{
+    public static class Constants
+    {
+        public const string HttpSchemeValue = "http";
+        public const string HttpsSchemeValue = "https";
+    }
+}
