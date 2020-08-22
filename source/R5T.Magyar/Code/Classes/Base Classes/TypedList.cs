531a4de7e9d411a7a4ac280a97e713112dee6356
David Coats <david@getsafetycone.com>
2020-08-22 14:08:13 -0700

    Improve TypedList<T> by implementing IEnumerable<T>, and added TypeList<T> extensions

---
+++ source/R5T.Magyar/Code/Classes/Base Classes/TypedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar
+{
+    /// <summary>
+    /// Allows a <see cref="List{T}"/> to be handled as distinct named type.
+    /// </summary>
+    public class TypedList<T> : IEnumerable<T>
+    {
+        public List<T> Values { get; } = new List<T>();
+
+
+        public TypedList()
+        {
+        }
+
+        /// <summary>
+        /// Adds input values to the internal list.
+        /// </summary>
+        public TypedList(IEnumerable<T> values)
+        {
+            this.Values.AddRange(values);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.Values.GetEnumerator();
+        }
+    }
+}
---
+++ source/R5T.Magyar/Code/Extensions/TypedListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.Magyar;
+
+
+namespace System
+{
+    /// <summary>
+    /// Placed in <see cref="System"/> namespace so as to be available anywhere you might have an instance of a <see cref="TypedList{T}"/>.
+    /// </summary>
+    public static class TypedListExtensions
+    {
+        public static void Add<T>(this TypedList<T> list, T value)
+        {
+            list.Values.Add(value);
+        }
+    }
+}
