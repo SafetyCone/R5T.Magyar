b090e0b4ca94781a6ff7de7a5c216bee55cb98bb
David Coats <david@getsafetycone.com>
2021-01-10 22:35:07 -0800

    Add DictionaryHelper

---
+++ source/R5T.Magyar/Code/Helpers/DictionaryHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar
+{
+    public static class DictionaryHelper
+    {
+        public static string GetDefaultKeyNotFoundExceptionMessage(string key)
+        {
+            var message = $"Key not found: '{key}'";
+            return message;
+        }
+
+        public static KeyNotFoundException GetDefaultKeyNotFoundException(string key)
+        {
+            var message = DictionaryHelper.GetDefaultKeyNotFoundExceptionMessage(key);
+
+            var exception = new KeyNotFoundException(message);
+            return exception;
+        }
+    }
+}
