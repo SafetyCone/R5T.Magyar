caf4b9732a171cac15a293f9a88b69ff8443c728
David Coats <david@getsafetycone.com>
2021-01-17 23:38:15 -0800

    Add ListExtensions

---
+++ source/R5T.Magyar/Code/Extensions/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar
+{
+    public static class ListExtensions
+    {
+        public static bool IsEmpty<T>(this List<T> list)
+        {
+            var isEmpty = list.Count < 1; // Assumes less-than is faster than equals.
+            return isEmpty;
+        }
+    }
+}
