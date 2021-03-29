14b5f6cdd89f98cc45f5673b95ca0d5bc21f4f57
David Coats <david@getsafetycone.com>
2021-03-28 22:02:40 -0700

    Add Has<T>

---
+++ source/R5T.Magyar/Code/Classes/Has.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Magyar
+{
+    public class Has<T>
+    {
+        public bool HasValue { get; }
+        public T Value { get; }
+
+
+        public Has(bool hasValue, T value)
+        {
+            this.HasValue = hasValue;
+            this.Value = value;
+        }
+
+        public Has(T value)
+            : this(EqualityComparer<T>.Default.Equals(value, default), value)
+        {
+        }
+
+        public Has()
+            : this(false, default)
+        {
+        }
+    }
+}
