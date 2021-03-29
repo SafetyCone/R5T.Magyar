935bdf106f22acf375cd8109e3873646e8914a41
David Coats <david@getsafetycone.com>
2021-03-28 22:02:16 -0700

    Add methods to get new UriBuilders with basic pre-configurations

--- source/R5T.Magyar/Code/Helpers/UriHelper.cs
+++ source/R5T.Magyar/Code/Helpers/UriHelper.cs
@@ -5,16 +5,46 @@ namespace R5T.Magyar
 {
     public static class UriHelper
     {
+        /// <summary>
+        /// Uses the <see cref="UriHelper.NewBetterDefaults"/> method.
+        /// </summary>
+        public static UriBuilder New()
+        {
+            var output = UriHelper.NewBetterDefaults();
+            return output;
+        }
+
+        /// <summary>
+        /// Uses the <see cref="UriHelper.NewBetterDefaults"/> methods to set better default values than <see cref="UriBuilder"/> parameterless constructor.
+        /// </summary>
+        public static UriBuilder NewBetterDefaults()
+        {
+            // The parameterless constructor for the UriBuilder sets defaults as specified by: https://docs.microsoft.com/en-us/dotnet/api/system.uribuilder.-ctor?view=netcore-2.2
+            var uriBuilder = new UriBuilder
+            {
+                Host = String.Empty,
+                Scheme = String.Empty,
+            };
+
+            return uriBuilder;
+        }
+
         public static UriBuilder NewHTTPS(string server)
         {
             var httpsSchemeName = UriScheme.HTTPS.ToName();
 
-            var uriBuilder = new UriBuilder
-            {
-                Scheme = httpsSchemeName
-            };
+            var uriBuilder = UriHelper.New();
+
+            uriBuilder.Scheme = httpsSchemeName;
+
             uriBuilder.SetServer(server);
             
+            return uriBuilder;
+        }
+
+        public static UriBuilder NewRelative()
+        {
+            var uriBuilder = UriHelper.NewBetterDefaults();
             return uriBuilder;
         }
     }
