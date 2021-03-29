bdd4397b78d2314c9a5bbf3f3a622581f8246ddf
David Coats <david@getsafetycone.com>
2021-03-28 22:03:00 -0700

    Add Selector static class to get a default LINQ selector instance

---
+++ source/R5T.Magyar/Code/Classes/Selector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+
+
+namespace R5T.Magyar
+{
+    public static class Selector
+    {
+        public static Expression<Func<T, T>> Default<T>()
+        {
+            Expression<Func<T, T>> output = x => x;
+            return output;
+        }
+    }
+}
