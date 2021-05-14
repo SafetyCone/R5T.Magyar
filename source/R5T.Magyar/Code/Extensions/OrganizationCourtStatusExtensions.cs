de4f6745328e7b79b7d4de2520837ee6ebf1a99f
Zeph Arvanitis <zeph.arvanitis@gmail.com>
2021-05-14 11:32:09 -0500

    Add OrganizationCourtStatus helpers

---
+++ source/R5T.Magyar/Code/Extensions/OrganizationCourtStatusExtensions.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+
+namespace R5T.Magyar
+{
+    public static class OrganizationCourtStatusExtensions
+    {
+        public static T ChooseByDecision<T>(this OrganizationCourtStatus status,
+            T approved,
+            T denied)
+        {
+            status.EnsureIsDecided();
+
+            if(status.IsApproved())
+            {
+                return approved;
+            }
+            else
+            {
+                return denied;
+            }
+        }
+
+        public static T ChooseByDecision<T>(this OrganizationCourtStatus status,
+            Func<T> approved,
+            Func<T> denied)
+        {
+            status.EnsureIsDecided();
+
+            if (status.IsApproved())
+            {
+                var approvedValue = approved();
+                return approvedValue;
+            }
+            else
+            {
+                var deniedValue = denied();
+                return deniedValue;
+            }
+        }
+
+        /// <summary>
+        /// Ensures an <see cref="OrganizationCourtStatus"/> is not <see cref="OrganizationCourtStatus.Pending"/>.
+        /// </summary>
+        public static void EnsureIsNotPending(this OrganizationCourtStatus status)
+        {
+            var isPending = status.IsPending();
+            if(isPending)
+            {
+                status.ThrowWasStillPendingException();
+            }
+        }
+
+        /// <summary>
+        /// Ensures an <see cref="OrganizationCourtStatus"/> has been decided (and is not still <see cref="OrganizationCourtStatus.Pending"/>).
+        /// </summary>
+        /// <remarks>
+        /// Uses <see cref="OrganizationCourtStatusExtensions.EnsureIsNotPending(OrganizationCourtStatus)"/>.
+        /// </remarks>
+        public static void EnsureIsDecided(this OrganizationCourtStatus status)
+        {
+            status.EnsureIsNotPending();
+        }
+
+        public static bool IsApproved(this OrganizationCourtStatus status)
+        {
+            var isApproved = status == OrganizationCourtStatus.Approved;
+            return isApproved;
+        }
+
+        public static bool IsDecided(this OrganizationCourtStatus status)
+        {
+            var isDecided = !status.IsPending();
+            return isDecided;
+        }
+
+        public static bool IsDenied(this OrganizationCourtStatus status)
+        {
+            var isDenied = status == OrganizationCourtStatus.Denied || status == OrganizationCourtStatus.DeniedWithReroute;
+            return isDenied;
+        }
+
+        public static T Switch<T>(this OrganizationCourtStatus organizationCourtStatus, T approved, T denied, T pending)
+        {
+            switch (organizationCourtStatus)
+            {
+                case OrganizationCourtStatus.Approved:
+                    return approved;
+
+                case OrganizationCourtStatus.Denied:
+                    return denied;
+
+                case OrganizationCourtStatus.Pending:
+                    return pending;
+
+                default:
+                    throw EnumerationHelper.SwitchDefaultCaseException(organizationCourtStatus);
+            }
+        }
+
+        public static bool IsPending(this OrganizationCourtStatus status)
+        {
+            var isPending = status == OrganizationCourtStatus.Pending;
+            return isPending;
+        }
+
+        public static void ThrowWasStillPendingException(this OrganizationCourtStatus status)
+        {
+            throw OrganizationCourtStatusHelper.GetApprovalWasStillPendingException();
+        }
+
+        public static string ToStringStandard(this OrganizationCourtStatus status)
+        {
+            var standardRepresentation = OrganizationCourtStatusHelper.ToStandardRepresentation(status);
+            return standardRepresentation;
+        }
+    }
+}
+
+
+namespace R5T.Magyar.Extensions
+{
+    public static class OrganizationCourtStatusExtensions
+    {
+        public static OrganizationCourtStatus ToOrganizationCourtStatus(this string standardRepresentation)
+        {
+            var status = OrganizationCourtStatusHelper.FromStandardRepresentation(standardRepresentation);
+            return status;
+        }
+    }
+}
---
+++ source/R5T.Magyar/Code/Helpers/OrganizationCourtStatusHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+
+namespace R5T.Magyar
+{
+    public static class OrganizationCourtStatusHelper
+    {
+        public static class StandardRepresentation
+        {
+            public const string Approved = "Approved";
+            public const string Denied = "Denied";
+            public const string DeniedWithReroute = "Denied with reroute";
+            public const string Pending = "Pending";
+        }
+
+
+        public static string ToStandardRepresentation(OrganizationCourtStatus organizationCourtStatus)
+        {
+            switch(organizationCourtStatus)
+            {
+                case OrganizationCourtStatus.Approved:
+                    return OrganizationCourtStatusHelper.StandardRepresentation.Approved;
+
+                case OrganizationCourtStatus.Denied:
+                    return OrganizationCourtStatusHelper.StandardRepresentation.Denied;
+
+                case OrganizationCourtStatus.DeniedWithReroute:
+                    return OrganizationCourtStatusHelper.StandardRepresentation.DeniedWithReroute;
+
+                case OrganizationCourtStatus.Pending:
+                    return OrganizationCourtStatusHelper.StandardRepresentation.Pending;
+
+                default:
+                    throw EnumerationHelper.SwitchDefaultCaseException(organizationCourtStatus);
+            }
+        }
+
+        public static OrganizationCourtStatus FromStandardRepresentation(string organizationCourtStatusStandardRepresenation)
+        {
+            switch(organizationCourtStatusStandardRepresenation)
+            {
+                case OrganizationCourtStatusHelper.StandardRepresentation.Approved:
+                    return OrganizationCourtStatus.Approved;
+
+                case OrganizationCourtStatusHelper.StandardRepresentation.Denied:
+                    return OrganizationCourtStatus.Denied;
+
+                case OrganizationCourtStatusHelper.StandardRepresentation.DeniedWithReroute:
+                    return OrganizationCourtStatus.DeniedWithReroute;
+
+                case OrganizationCourtStatusHelper.StandardRepresentation.Pending:
+                    return OrganizationCourtStatus.Pending;
+
+                default:
+                    throw EnumerationHelper.RepresentationUnrecognizedException<OrganizationCourtStatus>(organizationCourtStatusStandardRepresenation);
+            }
+        }
+
+
+        public static string GetApprovalWasStillPendingExceptionMessage()
+        {
+            var message = $"Approval status was still pending. Approval status must be '{OrganizationCourtStatus.Approved}', '{OrganizationCourtStatus.Denied}', or '{OrganizationCourtStatus.DeniedWithReroute}.";
+            return message;
+        }
+
+        public static ArgumentException GetApprovalWasStillPendingException(string argumentName = Strings.Empty)
+        {
+            var message = OrganizationCourtStatusHelper.GetApprovalWasStillPendingExceptionMessage();
+
+            var argumentNameIsNotEmpty = Strings.IsNotEmpty(argumentName);
+
+            var exception = argumentNameIsNotEmpty
+                ? new ArgumentException(message, argumentName)
+                : new ArgumentException(message);
+
+            return exception;
+        }
+    }
+}
