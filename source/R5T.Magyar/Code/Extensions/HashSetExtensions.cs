3c862258d2dc8b11fedda50d68a04fec2983c6e0
David Coats <david@getsafetycone.com>
2021-09-16 16:05:00 -0700

    Add HashSet<T> extensions.

---
+++ source/R5T.Magyar/Code/Extensions/HashSetExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace System
+{
+    public static class HashSetExtensions
+    {
+        public static void AddRange<T>(this HashSet<T> hashSet, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                hashSet.Add(item);
+            }
+        }
+    }
+}
