f1365dfe04835d31aa249345d271679e9fd4f28a
davidcoats <david@getsafetycone.com>
2023-03-23 02:29:19 -0700

    Interim changes.

--- source/R5T.Magyar/Code/Collections/Interfaces/IDistinctEnumerable.cs
+++ source/R5T.Magyar/Code/Collections/Interfaces/IDistinctEnumerable.cs
@@ -5,7 +5,7 @@ namespace System.Collections.Generic
 {
     /// <summary>
     /// Indicates (but does not guarantee) that an enumerable contains distinct elements.
-    /// /// Extension methods can be used to verify/ensure the enumerable contains unique if desirable (and the cost of computation is justified).
+    /// Extension methods can be used to verify/ensure the enumerable contains unique if desirable (and the cost of computation is justified).
     /// </summary>
     /// <remarks>
     /// Conceptually similar to <see cref="IReadOnlyCollection{T}"/>, but indicates the distict property instead of being read-only.
